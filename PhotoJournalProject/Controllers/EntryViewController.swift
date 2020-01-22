11da8bb5997621a00b93c4d48d629cf203e8189f
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-22 08:39:13 -0500

    Initial Commit

---
+++ PhotoJournalProject/Controllers/EntryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntryViewController.swift
+//  PhotoJournalProject
+//
+//  Created by Amy Alsaydi on 1/22/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    // outlets
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    // actions
+    
+    @IBAction func libraryButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+}
---
+++ PhotoJournalProject/Controllers/PhotoJournalController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PhotoJournalProject
+//
+//  Created by Amy Alsaydi on 1/22/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+    }
+
+
+}
+
+extension PhotoJournalController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+        
+        return cell 
+    }
+    
+    
+}
+
